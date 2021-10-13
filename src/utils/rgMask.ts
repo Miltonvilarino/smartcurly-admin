e0cd39e78dc944c556b820b36cc5252416baf4ac
Milton Vilarino Netto <miltonvilarino@gmail.com>
2021-10-13 03:15:10 -0300

    removendo alguns utils não usados

--- src/utils/rgMask.ts
+++
@@ -1,6 +0,0 @@
-export const MascaraRg = (v0: string, errChar = "?") => {
-  const v = v0.toUpperCase().replace(/[^\dX]/g, "");
-  return v.length === 8 || v.length === 9
-    ? v.replace(/^(\d{1,2})(\d{3})(\d{3})([\dX])$/, "$1.$2.$3-$4")
-    : errChar + v0;
-};
