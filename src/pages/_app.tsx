b8954ba6ea2c1a21fff58295355d86296f61d85b
Milton Vilarino Netto <miltonvilarino@gmail.com>
2021-10-13 03:30:23 -0300

    configurando firebase

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,24 @@
 import type { AppProps } from 'next/app'
+import firebase from "firebase/app";
+import "firebase/firestore";
 import CreateGlobalStyle from '../styles/global'
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyAnjtmX7CgTXk3FLdEJsOYYQ5UomqCGKXQ",
+    authDomain: "smart-curly-dev.firebaseapp.com",
+    projectId: "smart-curly-dev",
+    storageBucket: "smart-curly-dev.appspot.com",
+    messagingSenderId: "532790803830",
+    appId: "1:532790803830:web:e548af7da8100a032a8e57",
+    measurementId: "G-VJWRMZDPML",
+  };
+
+  if (!firebase?.getApp?.length) {
+    firebase?.initializeApp(firebaseConfig);
+  }
+
   return (
     <>
       <Component {...pageProps} />
